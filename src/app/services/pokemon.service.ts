c73b561c3ee2969502557f57a04201f1ecc56a78
jaumsw <j.calixtodegodoy@gmail.com>
2024-05-27 15:10:32 -0300

    feat and page: home page with pokemon list

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+  private apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=150';
+
+  constructor(private http: HttpClient) { }
+
+  getPokemons(): Observable<any> {
+    return this.http.get(this.apiUrl).pipe(
+      map((result: any) => {
+        return result.results.map((pokemon: { name: any; }, index: number) => ({
+          name: pokemon.name,
+          image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png`
+        }));
+      })
+    );
+  }
+}
